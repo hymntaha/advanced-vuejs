0f9e8990d2f935f135b5d4386feba6b42515b88f
taha uygun <tahauygun@gmail.com>
2020-03-04 22:49:26 -0800

    adding some searchbar methods

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,15 +1,18 @@
 <template>
-    <div>
-        <input type="text">
-    </div>
+  <div>
+    <input v-on:input="onInput"/>
+  </div>
 </template>
 
 <script>
-    export default {
-
-    };
+export default {
+  name: "SearchBar",
+  methods: {
+    onInput: function(event) {
+      console.log(event.target.value);
+    }
+  }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
