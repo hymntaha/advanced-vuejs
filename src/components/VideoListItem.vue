0e59b1085f611eec13658e1f4261847bd0490929
taha uygun <tahauygun@gmail.com>
2020-03-08 20:29:14 -0700

    thumbnail by computed property

--- src/components/VideoListItem.vue
+++ src/components/VideoListItem.vue
@@ -1,12 +1,17 @@
 <template>
     <li class="list-group-item">
-        <img :src="video.snippet.thumbnails.default.url" />
+        <img :src="thumbnailUrl" />
         {{video.snippet.title}}
     </li>
 </template>
 <script>
     export default {
       name: 'VideoListItem',
-      props:['video']
+      props:['video'],
+      computed:{
+        thumbnailUrl(){
+          return this.video.snippet.thumbnails.default.url
+        }
+      }
     }
 </script>
