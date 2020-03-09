c6b00b640546f57d136fb9072b3d144f92dc2a6e
taha uygun <tahauygun@gmail.com>
2020-03-08 20:08:10 -0700

    props validation

--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -1,11 +1,13 @@
 <template>
     <ul>
         VideoList
+        {{videos}}
     </ul>
 </template>
 <script>
 export default {
-  name: 'VideoList'
+  name: 'VideoList',
+  props: ['videos']
 }
 </script>
 <style>
