94ee383ee248c03d343816af80d3225b1a63a4cf
taha uygun <tahauygun@gmail.com>
2020-03-08 19:49:18 -0700

    adding a videolist component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div>
     <SearchBar @termChange="onTermChange"> </SearchBar>
+    <VideoList/>
   </div>
 </template>
 <script>
 import SearchBar from "./components/SearchBar";
+import VideoList from "./components/VideoList";
+
 import axios from 'axios';
 const API_KEY = 'API_KEY_HERE';
 
 export default {
   name: "App",
   components: {
-    SearchBar
+    SearchBar,
+    VideoList
   },
   methods:{
     onTermChange(searchTerm){
-      axios.get('https://googleapis.com/youtube/v3/search',{
+      axios.get('https://www.googleapis.com/youtube/v3/search', {
         params: {
           keY: API_KEY,
           type: 'video',
           part: 'snippet',
-          q:searchTerm
+          q: searchTerm
         }
-      })
+      }).then(response => console.log(response));
     }
   }
 };
