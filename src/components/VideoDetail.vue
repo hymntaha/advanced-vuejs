a0a4e0b0803d951e51fa61cdf774cf87d9b4c6e2
taha uygun <tahauygun@gmail.com>
2020-03-10 14:01:49 -0700

    referencing video title and description

--- src/components/VideoDetail.vue
+++ src/components/VideoDetail.vue
@@ -1,14 +1,22 @@
 <template>
-    <div v-if="video">
-        {{video.snippet.title}}
+  <div v-if="video">
+    <div class="details">
+      <h4>{{ video.snippet.title }}</h4>
+      <p>{{ video.snippet.description }}</p>
     </div>
+  </div>
 </template>
 <script>
-    export default {
-      name: 'VideoDetail',
-      props: ['video']
-    }
+export default {
+  name: "VideoDetail",
+  props: ["video"]
+};
 </script>
 <style>
-
+    .details {
+        margin-top: 10px;
+        padding: 10px;
+        border: 1px solid black;
+        border-radius: 4px;
+    }
 </style>
